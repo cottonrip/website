50d322a7cd4595223a2efa84a393f532c55b103a
kitku <kitku560@gmail.com>
2024-07-31 12:14:17 +0200

    vsda

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const snapZones = document.querySelectorAll('.snap-zone');
     const snapHeight = window.innerHeight;
 
+    let isScrolling;
+
     scrollContainer.addEventListener('scroll', () => {
-        let scrollPosition = scrollContainer.scrollTop;
-        let snapIndex = Math.round(scrollPosition / snapHeight);
-        
-        // Limit snapIndex to the number of snap zones
-        snapIndex = Math.min(snapZones.length - 1, Math.max(0, snapIndex));
-        
-        scrollContainer.scrollTo({
-            top: snapIndex * snapHeight,
-            behavior: 'smooth'
-        });
+        window.clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(() => {
+            let scrollPosition = scrollContainer.scrollTop;
+            let snapIndex = Math.round(scrollPosition / snapHeight);
+
+            // Limit snapIndex to the number of snap zones
+            snapIndex = Math.min(snapZones.length - 1, Math.max(0, snapIndex));
+
+            scrollContainer.scrollTo({
+                top: snapIndex * snapHeight,
+                behavior: 'smooth'
+            });
+        }, 66);
     });
 });
